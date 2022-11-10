985a239c13913cce0d665d03832d1f1a56d37115
KMohir <117655209+KMohir@users.noreply.github.com>
2022-11-10 12:19:46 +0500

    Update urls.py

--- qurilish/myshop/urls.py
+++ qurilish/myshop/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('BegzodMohirMuhammadAziz/', admin.site.urls),
+    path('quruvchiadmin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
     path('api/', include('drfsite.urls')),
 
